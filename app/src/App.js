d5fa9579f52064b9a9111da8ab253e63b83ececd
RobertPatru <86546157+RobertPatru@users.noreply.github.com>
2022-06-23 18:17:52 +0300

    e

--- app/src/App.js
+++ app/src/App.js
@@ -7,7 +7,7 @@ function Container() {
         <div className="container">
             <UpperSection />
             <SecondSection title={ 'Some non random text' } />
-            <ThirdSection />
+            <ThirdSection text={"This is an insipiring quote, or a testimonial from a customer. Maybe it's just filling up space, or maye people will actually read it. Who knows? All I know is that it looks nice."} />
         </div>
     );
 }
--- app/src/components/ThirdSection.jsx
+++ app/src/components/ThirdSection.jsx
@@ -1,12 +1,16 @@
-function ThirdSection () {
+function ThirdSection ( {text} ) {
     return (
         <div className="third-section default-sections-styles">
-            <div className="max-width">
-                <h2 className="text-align-center"><em>This is an insipiring quote, or a testimonial from a customer. Maybe it's just filling up space, or maye people will actually read it. Who knows? All I know is that it looks nice.</em></h2>
+            <div className="max-width add-flex add-flex-column">
+                <h2 className="text-align-center"><em> {text} </em></h2>
                 <span className="text-align-end"> - God if Thunder</span>
             </div>
         </div>
     )
+}
+
+ThirdSection.defaultProps = {
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro eveniet itaque sequi quaerat quod vel neque numquam, rem voluptatum, autem unde earum quia similique minus consequatur eos, amet iure! Saepe corporis temporibus ipsum ea exercitationem"
 }
 
 export default ThirdSection;
